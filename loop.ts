8aa524614b98bc8cd63b9f894ba5aa466c1d505d
Javeria Malik <javeriamalik6692@gmail.com>
2023-08-24 01:28:42 +0500

    Add files via upload

---
+++ loop.ts
@@ -0,0 +1,27 @@
+//while loop
+let i=2;
+++i;
+console.log(++i)
+for loop
+for(let i=0;i<10;i++){
+
+    console.log(i)
+}
+// decrement
+for(let i=10;i>=0;i--){
+
+    console.log(i,"jiya")
+}
+//three condition require for loop
+//increment,consition.initizalization 
+var i:number=4;
+do{
+    console.log("jiya");
+    i++
+}while(i<10)//it run atleast one time even value false
+//false
+var i:number=11;
+do{
+    console.log("jiya");
+    i++
+}while(i<10)
