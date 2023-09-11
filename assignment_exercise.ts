813698d9a84ea69028f5f6e8c81d483e2cc2a142
Javeria Malik <javeriamalik6692@gmail.com>
2023-09-11 23:09:38 +0500

    Add files via upload

---
+++ assignment_exercise.ts
@@ -0,0 +1,334 @@
+// // var nam:string="javeria";
+// // console.log("Hello",nam,"would u like to learn some python ")
+// // //using function
+// // function greeting(name:string){
+// //     console.log("Hello",nam,"would u like to learn some python ")
+// // }
+// // greeting("jiya");
+// // //question no 2 
+// // function titlecase(nam1:string){
+// //     console.log()
+// // }
+// //question no 3 or 4
+// // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// // Your output should look something like the following, including the quotation marks: 
+// // Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// // var author:string="Albert Einstein";
+// // var quote:string='“A person who never made a mistake never tried anything new.”'
+// // console.log(author,"once said,",quote)
+// // //Question no 6
+// // //# Store the person's name with whitespace characters
+// // // Store the person's name with whitespace characters
+// // let nameWithWhitespace: string = "\t  \n javeria \t  \n ";
+
+// // // Print the name with surrounding whitespace
+// // console.log("Name with whitespace:");
+// // console.log(nameWithWhitespace);
+
+// // // Strip the whitespace and print the name without surrounding whitespace
+// // let strippedName: string = nameWithWhitespace.trim();
+// // console.log("\nName after stripping whitespace:");
+// // console.log(strippedName);
+// //Question no 8 or 9
+// //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+// // Be sure to enclose your operations in print statements to see the results.
+// // console.log(5+5);
+// // console.log(5*5);
+// // console.log(5/5);
+// // console.log(5-5);
+// //Question no 9
+// //Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// // create a message that reveals your favorite number. Print that messa
+// var fvt_num:number=7;
+// console.log("you favourit num is",fvt_num)
+// //question no 11
+// //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file
+// //. Then write one sentence describing what the program does.
+// //it will print some of links
+// console.log(50+5);
+// console.log(50*5);
+// //Names: Store the names of a few of your friends in a array called names. 
+// //Print each person’s name by accessing each element in the list, one at a time.
+// //quesion no 11
+// //Names: Store the names of a few of your friends in a array called names.
+// // Print each person’s name by accessing each element in the list, one at a time.
+// // var friends:string[]=["farhan","javeria","arooj"]
+// // console.log(friends)
+// // console.log(friends[0]);
+// // for(let i=0;i<friends.length;i++){
+// //     console.log(i,friends[i])
+// // }
+// //question no 12
+// // Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same,
+// //  but each message should be personalized with the person’s name.
+// // var friends:string[]=["farhan","javeria","arooj"]
+// //  //console.log(friends)
+// //  //console.log(friends[0]);
+// //  for(let i=0;i<friends.length;i++){
+// //     console.log(i,friends[i],"good evening")
+// //  }
+// //Question no 13
+// // Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+// // and make a list that stores several examples. Use your list to print a series of statements about these items, 
+// // such as “I would like to own a Honda motorcycle.”
+// // var cars:string[]=["bmw","audi","honda"]
+// //  //console.log(friends)
+// //  //console.log(friends[0]);
+// //  for(let i=0;i<cars.length;i++){
+// //     console.log("i would like to buy",cars[i],"good evening")
+// //  }
+//  var car:string[]=["bmw","audi","honda"]
+//  //console.log(friends)
+//  //console.log(friends[0]);
+//  for(let i=0;i<car.length;i++){
+//     if (car[i]=="bmw"){
+//         console.log("i like to buy",car[i])
+//     }
+//     else if (car[i]=="audi"){
+//         console.log("i like to ride",car[i])
+//     }
+//     else if (car[i]=="honda"){
+//         console.log("i like to sale",car[i])
+//     }
+    
+
+    
+//  }
+// //question no 14
+// // Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+// //Make a list that includes at least three people you’d like to invite to dinner. 
+// // Then use your list to print a message to each person, inviting them to dinner.
+// var invite:string[]=["farhan","jiya","bia"]
+//  //console.log(friends)
+//  //console.log(friends[0]);
+//  for(let i=0;i<invite.length;i++){
+//     if (invite[i]=="farhan"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+//     else if (invite[i]=="jiya"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+//     else if (invite[i]=="bia"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+
+
+    
+//  }
+//  //question no 15
+//  var invite:string[]=["farhan","jiya","bia"]
+//  //console.log(friends)
+//  //console.log(friends[0]);
+//  for(let i=0;i<invite.length;i++){
+//     if (invite[i]=="farhan"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+//     else if (invite[i]=="jiya"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+//     else if (invite[i]=="bia"){
+//         console.log("i like to invite you on party",invite[i])
+//     }
+
+
+    
+//  }
+//  //question no 18
+//  // a. Store the locations in an array (not in alphabetical order)
+// const placesToVisit = [
+//     "Tokyo, Japan",
+//     "Paris, France",
+//     "New York City, USA",
+//     "Sydney, Australia",
+//     "Machu Picchu, Peru"
+//   ];
+  
+//   // b. Print the array in its original order
+//   console.log("Original Order:");
+//   console.log(placesToVisit);
+  
+//   // c. Print the array in alphabetical order without modifying the actual list
+//   const sortedAlphabetically = [...placesToVisit].sort();
+//   console.log("Alphabetical Order:");
+//   console.log(sortedAlphabetically);
+  
+// // d. Show that your array is still in its original order by printing it
+//   console.log("Original Order (unchanged):");
+//   console.log(placesToVisit);
+//    // e. Print the array in reverse alphabetical order without changing the order of the original list
+//   const sortedReverse = [...placesToVisit].sort().reverse();
+//   console.log("\nReverse Alphabetical Order:");
+//   console.log(sortedReverse);
+//   // f. Show that your array is still in its original order by printing it again
+//   console.log("Original Order:");
+//   console.log(placesToVisit);
+//   // g. Reverse the order of your list. Print the array to show that its order has changed.
+//   placesToVisit.reverse();
+//   console.log("Reversed Order:");
+//   console.log(placesToVisit);
+//   // h. Reverse the order of your list again. Print the list to show it’s back to its original order.
+//   placesToVisit.reverse();
+//   console.log("\nOriginal Order (restored):");
+//   console.log(placesToVisit);
+//   // i. Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+//   placesToVisit.sort();
+//   console.log("\nSorted Alphabetical Order:");
+//   console.log(placesToVisit);
+  
+//   // j. Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+//   placesToVisit.sort((a, b) => b.localeCompare(a)); // Sorting in reverse alphabetical order
+//   console.log("\nSorted Reverse Alphabetical Order:");
+//   console.log(placesToVisit);
+//   //question 24
+//   function isLowercaseString(str: string): boolean {
+//     for (const char of str) {
+//       if (char !== char.toLowerCase()) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+  
+//   const str1: string = "hello world";
+//   const str2: string = "Hello World";
+  
+//   console.log(isLowercaseString(str1)); // true
+//   console.log(isLowercaseString(str2)); // false
+
+// //question no 25
+// // Alien Colors #1: Imagine an alien was just shot down in a game.
+// //  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// //  a. Write an if statement to test whether the alien’s color is green. If it is,
+// //   print a message that the player just earned 5 points.
+// // b. Write one version of this program that passes the if test and another that fails. 
+// // (The version that fails will have no output.)
+// //part A    
+// // let allien_color:string="green";
+// // if (allien_color=="green"){
+// //     console.log("player will earn 5 points");
+    
+// // }
+// // //part B 
+// // let allien_colors:string="red";
+// // if (allien_colors=="green"){
+// //     console.log("player will earn 5 points");
+    
+// // }
+// //Question no 26
+// // Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// // a. If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+// // b. If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+// // c. Write one version of this program that runs the if block and another that runs the else block.
+// // let allien_color:string="green";
+// // if (allien_color=="green"){
+// //     console.log("player just 5 points");
+    
+// // }
+// // //part b
+// // let allien_colors:string="red";
+// // if (allien_colors!="green"){
+// //     console.log("player will just earn 10 points");
+    
+// // }
+// // //part 3
+// // let allienn_colors:string="blue";
+// // if (allienn_colors=="blue"){
+// //     console.log("you are right");
+    
+// // }
+// // else{
+// //     console.log("you are wrong");
+    
+// // }
+// // Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// // a. If the alien is green, print a message that the player earned 5 points.
+// // b. If the alien is yellow, print a message that the player earned 10 points.
+// // c. If the alien is red, print a message that the player earned 15 points.
+// // d. Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// let allienn_colors:string="yellow";
+// if (allienn_colors=="green"){
+//     console.log("you earn 5 points");
+    
+// }
+// else if(allienn_colors=="yellow"){
+//     console.log("you will earn 10 points");
+    
+
+// }
+// else if(allienn_colors=="red"){
+//     console.log("you will earn 15 points");
+//     }
+// else{
+//     console.log("you are wrong");
+    
+// }
+//Question no 28
+// let Age:number= 34;
+// if(Age<2){
+//     console.log("the person is still a baby")
+// }
+// else if(Age>=2 && Age<4){
+//     console.log("person is toddler");
+    
+// }
+// else if(Age>=4 && Age<13){
+//     console.log("person is kid");
+    
+// }
+// else if(Age>=13 && Age<20){
+//     console.log("person is teenager");
+    
+// }
+// else if(Age>=20 && Age<65){
+//     console.log("person is adult");
+    
+// }
+// else if(Age>=65){
+//     console.log("person is elder");
+    
+// }
+// else{
+//     console.log("person is not human ");
+    
+// }
+// Favorite Fruit: Make a array of your favorite fruits, 
+// and then write a series of independent if statements that check for certain fruits in your array
+// a. Make a array of your three favorite fruits and call it favorite_fruits.
+// b. Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, 
+// the if block should print a statement, such as You really like bananas!
+let fav_fruits:string[]=["cherry","bnana","orange"];
+if(fav_fruits.includes("cherry")){
+    console.log("its my favourit fruit chery")
+}
+if(fav_fruits.includes("bnana")){
+    console.log("its my favourit fruit banana")
+}
+if(fav_fruits.includes("orange")){
+    console.log("its my favourit fruit orange")
+}
+console.log("part b");
+let favourit_food:string="grapes";
+if(fav_fruits.includes("grape")){
+    console.log("its my favourit fruit orange")
+}
+else{
+    console.log("its not your favourit food");
+    
+}
+// 30. Hello Admin: Make a array of five or more usernames, including the name 'admin'.
+// Imagine you are writing code that will print a greeting to each user after they log in to a website. 
+//Loop through the array, and print a greeting to each user:
+// a. If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// b. Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let username:string[]=["admin","jiya","bisma","arooj","aosha"];
+for (let i =0;i<username.length;i++){
+    if(username[i]=="admin"){
+        console.log("welcome admin, lets see the ptrogress of you team ");
+        
+    }
+    else{
+        console.log(`hello ${username[i]},lets start your work`);
+        
+    }
+}
