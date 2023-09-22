08f0d0a58fb6aa31a9f041ebf78bf4127c6f3d14
Javeria Malik <javeriamalik6692@gmail.com>
2023-09-22 07:57:13 +0500

    Add files via upload

---
+++ promise.ts
@@ -0,0 +1,41 @@
+// type Cartype={
+//     price:number,
+//     color:string,
+//     print:()=>number
+// }
+// class Car{
+//     price:number=0;
+//     color:string="not define"
+//     constructor(color:string,price:number){
+//         this.color=color;
+//         this.price=price;
+//         console.log("constructor")
+//     }
+//     print=()=>{
+//         console.log("print");
+//         return 0
+//     }
+// }
+// let carB:Cartype=new Car("mehroon",98)
+// console.log(carB);
+console.log("start");
+
+function promisefunction(resolve:any,reject:any){
+    if(true){
+        resolve("successfully execute code")
+    }
+    else{
+        reject("something wrong")
+    }
+}
+new Promise(promisefunction);
+let result=new Promise(promisefunction)
+result.then((success)=>{
+    console.log("sucess",success);
+    
+})
+result.catch((error)=>{
+    console.log("error",error);
+    
+})
+console.log("stop");
