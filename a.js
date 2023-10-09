f2cef5df0e482deafebb56156ccd226d10207349
Javeria Malik <javeriamalik6692@gmail.com>
2023-10-09 23:37:09 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,10 @@
+"use strict";
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+;
+var colorName = Color[2];
+console.log(colorName);
